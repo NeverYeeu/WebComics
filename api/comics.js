115c19ceead8438e846462d46ded9d4b171ce7a2
Ng.V.Hoàng <134377540+NeverYeeu@users.noreply.github.com>
2023-10-02 21:16:06 +0700

    sua code

--- api/comics.js
+++ api/comics.js
@@ -3,13 +3,13 @@ const comics = [
 		id: 1,
 		nameComic: "Sekai Saikyou No Shinjuu Tsukai",
 		translator: "NeverYeeu",
-		nameChap: "Chapter 8",
+		nameChap: "Chapter 1",
 		linkImg: "https://i.postimg.cc/br14P1y6/Sekai-Saikyou-No-Shinjuu-Tsukai-2.jpg",
 		altImg: "Sekai_Saikyou_No_Shinjuu_Tsukai",
-		linkChap: "/chapter/ReadicCom-0001/chapter-8.html",
+		linkChap: "chapter/ReadicCom-0001/chapter-8.html",
 		updateChap: "06/08/23",
 		updateDay: "2",
-		linkComic: "/chapter/ReadicCom-0001/ReadicCom-0001.html",
+		linkComic: "chapter/ReadicCom-0001/ReadicCom-0001.html",
 		genre: ["adventure", "natural"],
 		viewer: 1,
 		liked: 0
@@ -21,10 +21,10 @@ const comics = [
 		nameChap: "Chapter 0",
 		linkImg: "https://i.postimg.cc/vm11YGJd/1669086278681-cua-hang-sung-thu-sieu-than2.png",
 		altImg: "Sủng_Thú_Siêu_Thần",
-		linkChap: "/chapter/ReadicCom-0002/chapter-0.html",
+		linkChap: "chapter/ReadicCom-0002/chapter-0.html",
 		updateChap: "16/08/23",
 		updateDay: "2",
-		linkComic: "/chapter/ReadicCom-0002/ReadicCom-0002.html",
+		linkComic: "chapter/ReadicCom-0002/ReadicCom-0002.html",
 		genre: ["manhwa", "comic", "action"],
 		viewer: 1,
 		liked: 0
@@ -34,10 +34,10 @@ const comics = [
 		nameComic: "Ta Có Một Sơn Trại",
 		linkImg: "https://i.postimg.cc/BnvDQhYx/001.jpg",
 		altImg: "Ta_Có_Một_Sơn_Trại",
-		linkComic: "/chapter/ReadicCom-0003/ReadicCom-0003.html",
+		linkComic: "chapter/ReadicCom-0003/ReadicCom-0003.html",
 		popularity: "Hot",
 		nameChap: "Chapter 1",
-		linkChap: "/chapter/ReadicCom-0003/chapter-1.html",
+		linkChap: "chapter/ReadicCom-0003/chapter-1.html",
 		updateChap: "22/08/23",
 		updateDay: "4",
 		genre: ["manhua", "comic", "action"],
@@ -49,10 +49,10 @@ const comics = [
 		nameComic: "Toàn Chức Pháp Sư",
 		linkImg: "https://i.postimg.cc/mrWVbmgZ/image.jpg",
 		altImg: "Toàn_Chức_Pháp_Sư",
-		linkComic: "/chapter/ReadicCom-0004/ReadicCom-0004.html",
+		linkComic: "chapter/ReadicCom-0004/ReadicCom-0004.html",
 		popularity: "Hot",
 		nameChap: "Chapter 1",
-		linkChap: "/chapter/ReadicCom-0004/chapter-1.html",
+		linkChap: "chapter/ReadicCom-0004/chapter-1.html",
 		updateChap: "22/08/23",
 		updateDay: "3",
 		genre: "Manhua",
@@ -67,7 +67,7 @@ const comics = [
 		linkComic: "/truyenTran",
 		popularity: "Hot",
 		nameChap: "chap1",
-		linkChap: "/chapter.html",
+		linkChap: "chapter.html",
 		updateChap: "06/08/23",
 		updateDay: "6",
 		genre: "Manga",
@@ -82,28 +82,12 @@ const comics = [
 		linkComic: "/truyenTran",
 		popularity: "Hot",
 		nameChap: "Chapter 1",
-		linkChap: "/chapter.html",
+		linkChap: "chapter.html",
 		updateChap: "04/08/23",
 		updateDay: "0",
 		genre: ["manhwa", "chuyển sinh", "action"],
 		viewer: 12,
 		liked: 10
-	  },
-	
-	  {
-		id: 7,
-		nameComic: "Làm Nông Dân Trong Tòa Tháp Thử Thách",
-		linkImg: "https://st.nettruyenmax.com/data/comics/26/lam-nong-dan-trong-toa-thap-thu-thach.jpg",
-		altImg: "Làm_Nông_Dân_Trong_Tòa_Tháp_Thử_Thách",
-		linkComic: "/truyenTran",
-		popularity: "Hot",
-		nameChap: "Chapter 21",
-		linkChap: "/chapter.html",
-		updateChap: "05/07/23",
-		updateDay: "3",
-		genre: ["anime", "comic", "action"],
-		viewer: 78,
-		liked: 0
 	  },
 	  {
 		id: 8,
@@ -113,27 +97,12 @@ const comics = [
 		linkComic: "/truyenTran",
 		popularity: "Hot",
 		nameChap: "Chapter 238",
-		linkChap: "/chapter.html",
+		linkChap: "chapter.html",
 		updateChap: "05/08/23",
 		updateDay: "2",
 		genre: "manhwa",
 		viewer: 42,
 		liked: 9
-	  },
-	  {
-		id: 9,
-		nameComic: "Nghịch Thiên Tà Thần",
-		linkImg: "https://st.nettruyenmax.com/data/comics/154/nghich-thien-ta-than.jpg",
-		altImg: "Nghịch_Thiên_Tà_Thần",
-		linkComic: "/truyenTran",
-		popularity: "Hot",
-		nameChap: "Chapter 640",
-		linkChap: "/chapter.html",
-		updateChap: "06/08/23",
-		updateDay: "1",
-		genre: "manhua",
-		viewer: 67,
-		liked: 35
 	  }
 ]
 export {comics}
@@ -142,70 +111,35 @@ const chapterComics = {
 	"readicCom-0000": [
 		{
 			"nameChapter": "Chapter 0",
-			"linkChapter": "/chapter/ReadicCom-0000/chapter-0.html",
+			"linkChapter": "chapter-0.html",
 			"timeDate": "22/08/2023"
 		}
 	],
 	"readicCom-0001": [
 		{
 		  "nameChapter": "Chapter 1",
-		  "linkChapter": "/chapter/ReadicCom-0001/chapter-1.html",
+		  "linkChapter": "chapter-1.html",
 		  "timeDate": "08/08/2023"
-		},
-		{
-		  "nameChapter": "Chapter 2",
-		  "linkChapter": "/chapter/ReadicCom-0001/chapter-2.html",
-		  "timeDate": "09/08/2023"
-		},
-		{
-		  "nameChapter": "Chapter 3",
-		  "linkChapter": "/chapter/ReadicCom-0001/chapter-3.html",
-		  "timeDate": "10/08/2023"
-		},
-		{
-		  "nameChapter": "Chapter 4",
-		  "linkChapter": "/chapter/ReadicCom-0001/chapter-4.html",
-		  "timeDate": "11/08/2023"
-		},
-		{
-		  "nameChapter": "Chapter 5",
-		  "linkChapter": "/chapter/ReadicCom-0001/chapter-5.html",
-		  "timeDate": "12/08/2023"
-		},
-		{
-		  "nameChapter": "Chapter 6",
-		  "linkChapter": "/chapter/ReadicCom-0001/chapter-6.html",
-		  "timeDate": "13/08/2023"
-		},
-		{
-		  "nameChapter": "Chapter 7",
-		  "linkChapter": "/chapter/ReadicCom-0001/chapter-7.html",
-		  "timeDate": "14/08/2023"
-		},
-		{
-		  "nameChapter": "Chapter 8: Có cái gì vậy",
-		  "linkChapter": "/chapter/ReadicCom-0001/chapter-8.html",
-		  "timeDate": "15/08/2023"
 		}
 	],
 	"readicCom-0002": [
 	{
 		"nameChapter": "Chapter 0",
-		"linkChapter": "/chapter/ReadicCom-0002/chapter-0.html",
+		"linkChapter": "chapter-0.html",
 		"timeDate": "22/08/2023"
 	}
 	],
 	"readicCom-0003": [
 	{
 		"nameChapter": "Chapter 1",
-		"linkChapter": "/chapter/ReadicCom-0003/chapter-1.html",
+		"linkChapter": "chapter-1.html",
 		"timeDate": "22/08/2023"
 	}
 	],
 	"readicCom-0004": [
 	{
 		"nameChapter": "Chapter 1",
-		"linkChapter": "/chapter/ReadicCom-0004/chapter-1.html",
+		"linkChapter": "chapter-1.html",
 		"timeDate": "22/08/2023"
 	}
 	]
